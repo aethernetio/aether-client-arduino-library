9d81b1b3530eeea63185b7a294a87f4a5b9c7b7e
BartolomeyKant <notevill@gmail.com>
2025-04-10 10:00:28 +0500

    update third_party

---
+++ src/third_party/etl/include/etl/crc.h
@@ -0,0 +1,88 @@
+///\file
+
+/******************************************************************************
+The MIT License(MIT)
+
+Embedded Template Library.
+https://github.com/ETLCPP/etl
+https://www.etlcpp.com
+
+Copyright(c) 2021 John Wellbelove
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+
+#ifndef ETL_CRC_INCLUDED
+#define ETL_CRC_INCLUDED
+
+#include "third_party/etl/include/etl/crc1.h"
+
+#include "third_party/etl/include/etl/crc8_ccitt.h"
+#include "third_party/etl/include/etl/crc8_cdma2000.h"
+#include "third_party/etl/include/etl/crc8_darc.h"
+#include "third_party/etl/include/etl/crc8_dvbs2.h"
+#include "third_party/etl/include/etl/crc8_ebu.h"
+#include "third_party/etl/include/etl/crc8_icode.h"
+#include "third_party/etl/include/etl/crc8_itu.h"
+#include "third_party/etl/include/etl/crc8_j1850_zero.h"
+#include "third_party/etl/include/etl/crc8_j1850.h"
+#include "third_party/etl/include/etl/crc8_maxim.h"
+#include "third_party/etl/include/etl/crc8_rohc.h"
+#include "third_party/etl/include/etl/crc8_wcdma.h"
+
+#include "third_party/etl/include/etl/crc16.h"
+#include "third_party/etl/include/etl/crc16_a.h"
+#include "third_party/etl/include/etl/crc16_arc.h"
+#include "third_party/etl/include/etl/crc16_aug_ccitt.h"
+#include "third_party/etl/include/etl/crc16_buypass.h"
+#include "third_party/etl/include/etl/crc16_ccitt.h"
+#include "third_party/etl/include/etl/crc16_cdma2000.h"
+#include "third_party/etl/include/etl/crc16_dds110.h"
+#include "third_party/etl/include/etl/crc16_dectr.h"
+#include "third_party/etl/include/etl/crc16_dectx.h"
+#include "third_party/etl/include/etl/crc16_dnp.h"
+#include "third_party/etl/include/etl/crc16_en13757.h"
+#include "third_party/etl/include/etl/crc16_genibus.h"
+#include "third_party/etl/include/etl/crc16_kermit.h"
+#include "third_party/etl/include/etl/crc16_maxim.h"
+#include "third_party/etl/include/etl/crc16_mcrf4xx.h"
+#include "third_party/etl/include/etl/crc16_modbus.h"
+#include "third_party/etl/include/etl/crc16_profibus.h"
+#include "third_party/etl/include/etl/crc16_riello.h"
+#include "third_party/etl/include/etl/crc16_t10dif.h"
+#include "third_party/etl/include/etl/crc16_teledisk.h"
+#include "third_party/etl/include/etl/crc16_tms37157.h"
+#include "third_party/etl/include/etl/crc16_usb.h"
+#include "third_party/etl/include/etl/crc16_x25.h"
+#include "third_party/etl/include/etl/crc16_xmodem.h"
+#include "third_party/etl/include/etl/crc16_m17.h"
+
+#include "third_party/etl/include/etl/crc32.h"
+#include "third_party/etl/include/etl/crc32_bzip2.h"
+#include "third_party/etl/include/etl/crc32_c.h"
+#include "third_party/etl/include/etl/crc32_d.h"
+#include "third_party/etl/include/etl/crc32_jamcrc.h"
+#include "third_party/etl/include/etl/crc32_mpeg2.h"
+#include "third_party/etl/include/etl/crc32_posix.h"
+#include "third_party/etl/include/etl/crc32_q.h"
+#include "third_party/etl/include/etl/crc32_xfer.h"
+
+#include "third_party/etl/include/etl/crc64_ecma.h"
+
+#endif
